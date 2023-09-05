469f32836b89245627d9b7ac8911048f94affb53
SanchezMaximo <maximosanchez420@gmail.com>
2023-09-05 16:05:44 -0300

    componente nuevo signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Login from "./Components/Login/Login";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import SignIn from "./Components/SignIn/SignIn";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
     )
--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,35 +1,34 @@
-import { useState } from "react"
-import "./Login.css"
+import { useState } from "react";
+import "./SignIn.css";
 
-const Login = () => {
+const SignIn = () => {
     const [nombreUsuario, setNombreUsuario] = useState();
     const [contraseniaUsuario, setContraseniaUsuario] = useState();
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         if(nombreUsuario.length >= 5 && nombreUsuario.length <=12){
-            alert(`Bienvenido ${nombreUsuario}`);
+            alert(`Usuario creado!`);
         } else {
             alert("su usuario debe contener entre 5 y 12 letras.");
         }
 
     }
-
   return (
     <div className="contenedorFormulario">
         <form onSubmit={handleSubmit} className="formularioLogin">
-            <div className="iniciarSesion">
-                <h3>INICIAR SESIÓN</h3>
+            <div className="registrarse">
+                <h3>REGISTRARSE</h3>
             </div>
             <label className="labelUsuario">Usuario</label>
-            <input className="inputUsuario" type="text"  name="usuario"  placeholder="ingrese su usuario" value={nombreUsuario} onChange={(e) => setNombreUsuario(e.target.value)}/>
+            <input className="inputUsuario" type="text"  name="usuario"  placeholder="ingrese un usuario" value={nombreUsuario} onChange={(e) => setNombreUsuario(e.target.value)}/>
             <label className="labelContrasenia">Contraseña</label>
-            <input className="inputContrasenia" type="password" name="contraseña"  placeholder="ingrese su contraseña" value={contraseniaUsuario} onChange={(e) => setContraseniaUsuario(e.target.value)}/>
+            <input className="inputContrasenia" type="password" name="contraseña"  placeholder="ingrese una contraseña" value={contraseniaUsuario} onChange={(e) => setContraseniaUsuario(e.target.value)}/>
             <button type="submit" className="botonFormulario">Enviar</button>
         </form>
-        <p className="textoRegistroCuenta">¿No tienes una cuenta? <b>Regístrate.</b></p>
+        <p className="textoRegistroCuenta">¿Ya tienes una cuenta? <b>Inicia sesión.</b></p>
     </div>
   )
 }
 
-export default Login
+export default SignIn
