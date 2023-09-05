66b8225fd70640ecedc107ffe9b9919d336c136a
BoFo <brunofazio466@gmail.com>
2023-09-05 10:04:44 -0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Login from "./Components/Login/Login";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 const App = () => {
   return (
-    <> 
-    <Login />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
     )
 }
 
