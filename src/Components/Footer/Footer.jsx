9f900074d2eb3574924130165c04a28bfb6ca099
SanchezMaximo <maximosanchez420@gmail.com>
2023-09-06 22:55:45 -0300

    css footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,26 +3,26 @@ import "./Footer.css";
 const Footer = ({children}) => {
   return (
     <section className="footer">   
-        {children}
+    {children}
        <div className="itbankFooter"> 
             <p id="itBankFooter">ITBANK</p>
             <p id="paisFooter">🇦🇷 Argentina</p>
        </div>
        <div className="redesYLegalesFooter">
-        <div className="redesFooter">
-                <p id="instagram">Instagram</p>
-                <p id="linkedin">LinkedIn</p>
-        </div>
-        <div className="legalFooter">
-            <ul className="listaLegal">
-                <li>Términos y condiciones</li>
-                <li>Privacidad y confidencialidad</li>
-                <li>Marcas registradas</li>
-            </ul>
-        </div>
+            <div className="redesFooter">
+                    <p id="instagram">Instagram</p>
+                    <p id="linkedin">LinkedIn</p>
+            </div>
+            <div className="legalFooter">
+                <ul className="listaLegal">
+                    <li>Términos y condiciones</li>
+                    <li>Privacidad y confidencialidad</li>
+                    <li>Marcas registradas</li>
+                </ul>
+            </div>
        </div>
       <div className="derechosReservadosFooter">
-        <p>Todos los Derechos Reservados. Prohibida su reproducción total o parcial. Los usuarios se obligan a cumplir con los Términos y Condiciones del Sitio de ITBANK. <br/> Copyright © 2023 <b>ITBANK</b>.</p>
+        <p>Todos los derechos reservados. Prohibida su reproducción total o parcial. Los usuarios se obligan a cumplir con los términos y condiciones del sitio de ITBANK. <br/> Copyright © 2023 <b>ITBANK</b>.</p>
       </div>
     </section>
   )
